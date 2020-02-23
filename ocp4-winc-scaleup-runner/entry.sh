ae31115eb3f83b9c7d9d635b89e84059b7f9d6c6
sgao <sgao@redhat.com>
2020-02-23 23:48:05 +0800

    debug

--- ocp4-winc-scaleup-runner/entry.sh
+++ ocp4-winc-scaleup-runner/entry.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # export RUNNER_WORKER_DIR=${WORKSPACE}/v4-testing-misc/v4-scaleup-next/ocp4-winc-scaleup-runner/${OCP_VERSION}
-export RUNNER_WORKER_DIR=${WORKSPACE}/winc-scaleup/ocp4-winc-scaleup-runner/${OCP_VERSION}
+export RUNNER_WORKER_DIR=${WORKSPACE}/${JOB_NAME}/ocp4-winc-scaleup-runner/${OCP_VERSION}
 export WINC_REPO="windows-machine-config-bootstrapper"
 # export KUBECONFIG_DIR=${WORKSPACE}/installer
 
@@ -11,18 +11,20 @@ cd ${RUNNER_WORKER_DIR}
   # inventory generation
   ansible-playbook generate_inventory.yaml -v
   # export KUBECONFIG="${KUBECONFIG_DIR}/auth/kubeconfig"
-  export KUBECONFIG="${WORKSPACE}/kubeconfig"
+  # wget kubeconfig
+  # export KUBECONFIG="${RUNNER_WORKER_DIR}/kubeconfig"
   cat inventory
 
   # Check windows connection
   ansible win -i inventory -m win_ping -v
 
   # TODO: setup go env here or in scaleup_pre_hook.yaml
+  git clone https://github.com/openshift/windows-machine-config-bootstrapper --depth=1
 
   case "${OPERATION}" in
   "SCALEUP" )
     ansible-playbook -i inventory scaleup_pre_hook.yaml -v
-    ansible-playbook -i inventory ${WORKSPACE}/${WINC_REPO}/tools/ansible/tasks/wsu/main.yml -v
+    ansible-playbook -i inventory ${RUNNER_WORKER_DIR}/${WINC_REPO}/tools/ansible/tasks/wsu/main.yml -v
     ansible-playbook -i inventory scaleup_post_hook.yaml -v
   ;;
   * )

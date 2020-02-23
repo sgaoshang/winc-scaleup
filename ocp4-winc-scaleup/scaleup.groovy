b4b638c4a940ff52215ff43d9f10e9ff33edb48a
sgao <sgao@redhat.com>
2020-02-24 02:32:54 +0800

    debug

--- ocp4-winc-scaleup/scaleup.groovy
+++ ocp4-winc-scaleup/scaleup.groovy
@@ -16,38 +16,13 @@ pipeline {
             }
         }
 
-        // stage('Clone the repo') {
-        //     steps {
-        //         checkout changelog: false,
-        //             poll: false,
-        //             scm: [
-        //                 $class: 'GitSCM', 
-        //                 branches: [[name: "master"]],
-        //                 doGenerateSubmoduleConfigurations: false,
-        //                 extensions: [
-        //                     [$class: 'CloneOption', noTags: true, reference: '', shallow: true],
-        //                     [$class: 'PruneStaleBranch'],
-        //                     [$class: 'CleanCheckout'],
-        //                     [$class: 'IgnoreNotifyCommit'],
-        //             ],
-        //             submoduleCfg: [],
-        //             userRemoteConfigs: [[
-        //               credentialsId: 'c9fb86e4-bd29-425f-a834-16ef21009d84',
-        //               name: 'origin',
-        //               refspec: "+refs/heads/master:refs/remotes/origin/master",
-        //               url: 'ssh://openshift-jenkins@code.engineering.redhat.com:22/openshift-misc'
-        //               ]]
-        //             ]
-        //     }
-        // }
-
         // TODO: IPI only now
         stage('Fetch artifacts from upstream job') {
             environment {
                 BUILD_NUMBER = "${env.FLEXY_BUILD_NUMBER}"
             }
             steps {
-                copyArtifacts filter: 'workdir/kubeconfig',
+                copyArtifacts filter: 'workdir/kubeconfig, workdir/vminfo.yml',
                               fingerprintArtifacts: true,
                               flatten: true,
                               projectName: 'Launch Environment Flexy',
@@ -73,36 +48,19 @@ pipeline {
         }
 
         stage ('Run scale-up job') {
-            // environment {
-            //     CLUSTER_KUBECONFIG_LOCATION = "${readYaml(file: "cluster_info.yaml")["INSTALLER"]["KUBECONFIG_URL"]}"
-            //     PLATFORM = "${readYaml(file: "cluster_info.yaml")["GENERAL"]["PLATFORM"]}"
-            //     V_X = "${readYaml(file: "cluster_info.yaml")["INSTALLER"]["VER_X"]}"
-            //     V_Y = "${readYaml(file: "cluster_info.yaml")["INSTALLER"]["VER_Y"]}"
-            //     NETWORK_TYPE = "${readYaml(file: "cluster_info.yaml")["GENERAL"]["NETWORK_TYPE"]}"
-            //     BASTION = "${readYaml(file: "cluster_info.yaml")["INSTALLER"]["BASTION"]}"
-            //     FLEXY_VARS = "${readYaml(file: "cluster_info.yaml")["TEMPLATE_VARS"]}"
-            //     scaleup_vars = "${env.EXTRA_VARS}\nplaceholder: true"
-            // }
+            environment {
+                BUILD_NUMBER = "${env.FLEXY_BUILD_NUMBER}"
+            }
             steps {
                 script {
-                    def rhelProps = readJSON file: env.OUTPUT_PATH
-                    // env.RHEL_WORKER_LIST = rhelProps.RHEL_WORKER_LIST
-                    def runner_vars = ""
-                    runner_vars += "ssh_user: ${rhelProps.ANSIBLE_USER.value}\n"
-                    runner_vars += "platform: ${env.PLATFORM}\n"
-                    runner_vars += "network_type: ${env.NETWORK_TYPE}\n"
-                    runner_vars += "bastion: ${env.BASTION}"
+                    def rhelProps = readJSON file: windows-node-installer.json
+                    KUBECONFIG_URL=https://openshift-qe-jenkins.rhev-ci-vms.eng.rdu2.redhat.com/job/Launch%20Environment%20Flexy/${env.FLEXY_BUILD_NUMBER}/artifact/workdir/install-dir/auth/kubeconfig
                     build job: 'ocp4-rhel-scaleup-runner', parameters: [
                         string(name: 'OPERATION', value: "SCALEUP"),
-                        string(name: 'RHEL_WORKERS', value: "${rhelProps.RHEL_WORKER_LIST.value}"),
-                        string(name: 'KUBECONFIG_URL', value: "${env.CLUSTER_KUBECONFIG_LOCATION}"),
-                        string(name: 'OCP_VERSION', value: "${env.V_X}.${env.V_Y}"),
-			[$class: 'LabelParameterValue', name: 'JENKINS_SLAVE_LABEL', label: "${params.JENKINS_SLAVE_LABEL}"],
-                        text(name: 'EXTRA_VARS', value: """
-${env.FLEXY_VARS}
-${env.scaleup_vars}
-${runner_vars}
-""")
+                        string(name: 'WINC_WORKERS', value: "TODO: generate windows worker list"),
+                        string(name: 'KUBECONFIG_URL', value: "${KUBECONFIG_URL}"),
+                        string(name: 'OCP_VERSION', value: "4.4"),
+                        [$class: 'LabelParameterValue', name: 'JENKINS_SLAVE_LABEL', label: "${params.JENKINS_SLAVE_LABEL}"],
                     ], wait: true
                 }
             }
@@ -110,7 +68,7 @@ ${runner_vars}
     }
     post {
         always {
-            archiveArtifacts artifacts: 'terraform.tfstate.d/**/*.*', fingerprint: true
+            archiveArtifacts artifacts: 'windows-node-installer.json', fingerprint: true
             cleanWs()
         }
     }

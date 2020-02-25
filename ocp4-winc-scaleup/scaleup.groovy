065e670a49c43f3b701ea827fca86aae66946715
sgao <sgao@redhat.com>
2020-02-26 03:58:37 +0800

    debug

--- ocp4-winc-scaleup/scaleup.groovy
+++ ocp4-winc-scaleup/scaleup.groovy
@@ -51,7 +51,7 @@ pipeline {
                         fi
                         rm -rf wni; wget ${WNI_URL} --quiet
                         chmod 777 wni
-                        wni_output=`./wni aws create --kubeconfig kubeconfig --credentials ${AWS_CREDS} --credential-account default --instance-type m5a.large --ssh-key openshift-qe --private-key ~/.ssh/openshift-qe.pem' 2>&1`
+                        wni_output=`./wni aws create --kubeconfig kubeconfig --credentials ${AWS_CREDS} --credential-account default --instance-type m5a.large --ssh-key openshift-qe --private-key ~/.ssh/openshift-qe.pem 2>&1`
                         echo \$wni_output | grep -Po "(?<=Windows instance created at ).*(?= using )|(?<= using ).*(?= as user and )|(?<= as user and ).*(?= password)" | tr "\n" "," > winc_workers.txt
                         """
                       }
